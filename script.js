3db5134fe43452760e67582941a7bdb4e8ac7872
Deepanshi <singhalnhy01@gmail.com>
2024-12-26 19:24:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const loginPopup = document.getElementById("loginPopup");
+
+function openPopup() {
+    loginPopup.style.display = "flex";
+}
+
+function closePopup() {
+    loginPopup.style.display = "none";
+}
